ac165471b1e9b508ef429e439664585c6be4ba07
lazyWolf13 <117087846+lazyWolf13@users.noreply.github.com>
2024-08-06 15:06:45 +0700

    Add files via upload
    tugas modul 1

---
+++ tugas_modul1/Tugas_modul1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas_modul1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Tugas_modul1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tugas1 coba=new tugas1();
+        coba.setVisible(true);
+    }
+    
+} 
